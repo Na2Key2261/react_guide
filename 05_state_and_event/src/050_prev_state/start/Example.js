3dfffcb79d36f525a3cbf7e56f085f1d6fedef0e
natsuki <natsuki.97285@gmail.com>
2024-03-29 12:25:57 +0900

    add

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,30 @@
 import { useState } from "react";
 
+
+
 const Example = () => {
-  
+  const [count, setCount] = useState(0);
+  const countUp = () => {
+    setCount(count + 1);
+    setCount(prevstate /* 現在のstateの値 */ => {
+      return prevstate + 1; /* 次のstateの値 */
+    } );
+    console.log(count);
+  };
+  const countDown = () => {
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <p>現在のカウント数: {count}</p>
+      <button
+        onClick={countUp}
+      >+</button>
+      <button
+        onClick={countDown}
+      >-</button>
+    </>
+  );
 };
 
 export default Example;
