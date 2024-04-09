7813eaa007641b55dee6eba027042f8aaeef9ba8
natsuki <natsuki.97285@gmail.com>
2024-04-09 13:07:19 +0900

    add

--- 07_styling_component/src/040_css_in_js/start/Example.js
+++ 07_styling_component/src/040_css_in_js/start/Example.js
@@ -1,18 +1,72 @@
+
 import { useState } from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  margin-inline: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  margin: 10px auto;
+  font-weight: bold;
+  cursor: pointer;
+  text-align: center;
+  line-height: 60px;
+  /* POINT valueを関数にすることで、引数にpropsを受け取ることができる。
+  { isSelected } の部分を変更することで受け取る名前を変更することができる。
+   */
+  background-color: ${({ isSelected }) => (isSelected ? "pink" : "darkcyan")};
+  transition: all 0.3s ease-out;
+
+  /* POINT 疑似クラスの追加 */
+  :hover,
+  :active {
+    opacity: 0.7;
+    transform: scale(1.1);
+  }
+  span {
+    color: purple;
+  }
+  /* POINT メディアクエリ */
+  @media (max-width: 600px) {
+    border-radius: 0;
+  }
+
+  :global {
+    background-color: black;
+  }
+`;
+
+const StyledSubButton = styled(StyledButton)`
+  background-color: ${({ isSelectedSub }) =>
+    isSelectedSub ? "crimson" : "coral"};
+`;
+
+const StyledOliveButton = styled(StyledButton)`
+  background-color: olive;
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
+  const [isSelectedSub, setIsSelectedSub] = useState(false);
 
-  const clickHandler = () => setIsSelected((prev) => !prev);
+  const onClickHandler = () => setIsSelected(!isSelected);
+  const onClickSubHandler = () => setIsSelectedSub(!isSelectedSub);
 
   return (
     <>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
+      <StyledButton isSelected={isSelected} onClick={onClickHandler}>
         ボタン
-      </button>
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
-      </div>
+      </StyledButton>
+      <StyledSubButton
+        isSelectedSub={isSelectedSub}
+        onClick={onClickSubHandler}
+      >
+        サブボタン
+      </StyledSubButton>
+      <StyledOliveButton><span>オリーブ</span></StyledOliveButton>
     </>
   );
 };
