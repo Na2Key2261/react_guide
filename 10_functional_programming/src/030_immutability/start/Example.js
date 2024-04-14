dc6cb0bfcfa0a1f9301cced529d5717b0f557616
natsuki <natsuki.97285@gmail.com>
2024-04-14 17:16:39 +0900

    add

--- 10_functional_programming/src/030_immutability/start/Example.js
+++ 10_functional_programming/src/030_immutability/start/Example.js
@@ -1,8 +1,18 @@
 const Example = () => {
+  const num = { val: 2 }
+  
+  const double = (num) => {
+    const newNum = { val: num.val * 2 };
+    // newNum.val = num.val * 2;
+    return newNum;
+  }
+
+  const newNum = double(num);
+  console.log('newNum', newNum, 'num', num)
+  console.log(newNum === num);
   return (
     <>
-      <h3>不変性（Immutability）</h3>
-      <p>引数で渡ってきたオブジェクトを変更しない！</p>
+      <div>Immutability</div>
     </>
   );
 };
