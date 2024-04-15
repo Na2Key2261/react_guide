f967e7a438c62dfabe6c60aa21eed54b82d6acd5
natsuki <natsuki.97285@gmail.com>
2024-04-15 14:58:22 +0900

    add

--- 10_functional_programming/src/050_react_data_procedure/start/Example.js
+++ 10_functional_programming/src/050_react_data_procedure/start/Example.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+let value = 0;
 const Example = () => {
+  // 関数コンポーネントはJSXを作成する場所なので、それ以外の処理は書かない。
+
+  // 状態はstateに分離する
   const [ state, setState ] = useState(0);
+
   const increment = () => {
     setState(state + 1);
   }
