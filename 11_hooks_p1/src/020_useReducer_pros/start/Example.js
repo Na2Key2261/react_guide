419773d9205e5e18ba4ae5ceda1e08d7668c190f
natsuki <natsuki.97285@gmail.com>
2024-04-18 19:08:34 +0900

    add

--- 11_hooks_p1/src/020_useReducer_pros/start/Example.js
+++ 11_hooks_p1/src/020_useReducer_pros/start/Example.js
@@ -9,27 +9,29 @@ import { useReducer, useState } from "react";
 
 // 純粋性（純粋関数）
 // 特定の引数に特定の戻り値
-const reducer = (prev, { type, step }) => {
-  switch (type) {
-    case "+":
-      return prev + step;
-    case "-":
-      return prev - step;
-    default:
-      throw new Error('不明なactionです。')
-  }
-}
-
-// 不変性（Immutability）
 const Example = () => {
   const [state, setState] = useState(0);
-  const [rstate, dispatch] = useReducer(reducer, 0);
-  
-  const step = 2;
+  const [rstate, dispatch] = useReducer((prev, { type, step }) => {
+    switch (type) {
+      case "+":
+        return prev + step;
+      case "-":
+        return prev - step;
+      default:
+        throw new Error('不明なactionです。')
+    }
+    // if (action === "+") {
+    //   return ++prev;
+    // } else if (action === "-") {
+    //   return --prev;
+    // }
+  }, 0);
+
   const countUp = () => {
-    setState((prev) => {
-      return prev + step
-    });
+    setState((prev) => ++prev);
+  };
+  const countDown = () => {
+    setState((prev) => --prev);
   };
   const rcountUp = () => {
     dispatch({ type: "+", step: 2 });
@@ -42,6 +44,7 @@ const Example = () => {
       <div>
         <h3>{state}</h3>
         <button onClick={countUp}>+</button>
+        <button onClick={countDown}>-</button>
       </div>
       <div>
         <h3>{rstate}</h3>
