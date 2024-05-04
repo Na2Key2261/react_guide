7f34955d534bea1ab2990229f87c9911d0f32166
natsuki <natsuki.97285@gmail.com>
2024-05-04 22:27:43 +0900

    add

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 type Todo = {
   value: string;
+  readonly id: number;
 };
 
 export const App = () => {
@@ -17,10 +18,24 @@ export const App = () => {
 
     const newTodo: Todo = {
       value: text,
+      id: new Date().getTime(),
     };
 
     setTodos((todos) => [newTodo, ...todos]);
     setText('');
+  };
+
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, value };
+        }
+        return todo;
+      });
+
+      return newTodos;
+    });
   };
 
   return (
@@ -34,6 +49,19 @@ export const App = () => {
         <input type="text" value={text} onChange={(e) => handleChange(e)} />
         <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.value}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
