ba2bcaa15de2c0c1e773cc5bd4b8a469a48f216b
Na2Key2261 <natsuki.97285@gmail.com>
2024-05-25 20:55:59 +0900

    add

--- 14_class_component/src/020_react_class_comp/start/Example.js
+++ 14_class_component/src/020_react_class_comp/start/Example.js
@@ -1,9 +1,27 @@
+　
 import { Component } from "react";
+
+const FnComp = ({name}) => {
+  return (
+    <h3>{name} Function Component</h3>
+  )
+}
+
+class Welcome extends Component {
+  render() {
+    return (
+       <h3>{this.props.name} Welcome Component</h3>
+    );
+  }
+}
 
 class Example extends Component {
   render() {
     return (
-      <h3>I'm Class Component</h3>
+      <>
+        <Welcome name={'Hello'}/>
+        <FnComp name={'Hello'} />
+      </>
     );
   }
 }
